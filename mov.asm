976f731c0c6e7bc177d9298c0a7794807d8299c2
Storm00212 <paulmuyalikhams@gmail.com>
2026-09-10 09:48:20 +0300

    rename

--- mov.asm
+++
@@ -1,9 +0,0 @@
-.intel_syntax noprefix
-.global _start
-.text 
-_start
-
-mov rax, 5   ; moves 5 to the register.
-mov rbx, rax ;moves the value of rax to rbx.
-mov rax,  0x4ffffffffff7  ; can also move hexadecimal values.
-mov rax, [0000500000000088] ; stores the data located in that address.
