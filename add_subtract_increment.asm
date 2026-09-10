e97a466c18d952e0025a6e2bd9b9e1123efb16ec
Storm00212 <paulmuyalikhams@gmail.com>
2026-09-10 09:51:00 +0300

    addition and subtraction

---
+++ add_subtract_increment.asm
@@ -0,0 +1,11 @@
+.intel_syntax noprefix
+.global _start
+.text 
+_start
+
+
+mov rax, 5
+mov rbx, 10
+
+add rax, rbx ;adds rbx to the value of rax.
+sub rax, rbx ;subtracts rbx from the value of rax.
