9e5841d9592770d8382ed0e01a1f90435aa720a4
Storm00212 <paulmuyalikhams@gmail.com>
2026-09-10 09:53:13 +0300

    increment and decrement

--- add_subtract_increment.asm
+++ add_subtract_increment.asm
@@ -9,3 +9,12 @@ mov rbx, 10
 
 add rax, rbx ;adds rbx to the value of rax.
 sub rax, rbx ;subtracts rbx from the value of rax.
+
+inc rax
+inc rax
+
+inc rax ; increments rax 3 times.
+
+dec rax
+dec rax
+dec rax ; decrements rax 3 times.
